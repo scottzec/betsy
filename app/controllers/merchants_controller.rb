bcfe9d0c8a689f726c367a1ddd88c5618a5883c7
Pauline Chane <pauline.chane@gmail.com>
2020-11-18 11:14:32 -0800

    added show and index functions, still need edit/update and require login controller filter

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,4 +1,19 @@
 class MerchantsController < ApplicationController
+  def index
+    @merchants = Merchant.all
+  end
+
+  def show
+    @merchant = Merchant.find_by(id: params[:id])
+    if @merchant.nil?
+      flash[:error] = "Sorry, merchant not found -- Check out our current merchants below!"
+      redirect_back fallback_location: merchants_path
+    else
+      @orderitems = @merchant.orderitems
+    end
+  end
+
+  # the new function is now handled by GitHub via OAuth. We will only need a create.
   def create
     auth_hash = request.env["omniauth.auth"]
     @merchant = Merchant.find_by(uid: auth_hash[:uid], provider: "github")
