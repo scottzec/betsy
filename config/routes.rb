b73d654c8930167046e6a5b4c85a2fd634ce13c9
Leah Scott-Zechlin <scottzec@gmail.com>
2020-11-18 17:49:23 -0800

    changed routes and added nested orderitem to rproducts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'products#index'
-  resources :merchants
-  resources :products do
-    resources :reviews
+
+  resources :merchants do
+    resources :products
   end
-  resources :orders
+
+  resources :products do
+      resources :reviews
+      resources :orderitem
+  end
+
+  resources :orders   # , only: [:show, :edit, :update]
 
 end
