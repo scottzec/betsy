913d1e7427e120fa0299ef8b76a5d23aaaff6179
Pauline Chane <pauline.chane@gmail.com>
2020-11-18 18:22:19 -0800

    commented out pseudo code for merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,16 +2,50 @@ class Merchant < ApplicationRecord
   has_many :products
   has_many :orderitems, through: :products
 
-  validates :username, uniqueness: true
-  
-  def self.build_from_github(auth_hash)
-    merchant = Merchant.new
-    merchant.uid = auth_hash[:uid]
-    merchant.provider = "github"
-    # merchant can change name later
-    merchant.username = auth_hash["info"]["name"]
-    merchant.email = auth_hash["info"]["email"]
+  # validates :username, uniqueness: true
+  # validates :email, uniqueness: true
+  #
+  # def self.build_from_github(auth_hash)
+  #   merchant = Merchant.new
+  #   merchant.uid = auth_hash[:uid]
+  #   merchant.provider = "github"
+  #   # merchant can change name later
+  #   merchant.username = auth_hash["info"]["name"]
+  #   merchant.email = auth_hash["info"]["email"]
+  #
+  #   return merchant
+  # end
+  #
+  # # total revenue based on completed orders
+  # def total_revenue
+  #   @orderitems = self.orderitems
+  #   return 0 if @orderitems.empty?
+  #
+  #   total = 0
+  #   @orderitems.each do |orderitem|
+  #     order = Order.find_by(id: orderitem.order_id)
+  #     product = Product.find_by(id: orderitem.product_id)
+  #     if order.status.downcase == 'completed'
+  #       total += orderitem.quantity * price
+  #     end
+  #   end
+  #
+  #   return total
+  # end
+  #
+  # def all_orders
+  #   @orderitems = self.orderitems
+  #   return nil if @orderitems.empty?
+  #   allorders = Hash.new
+  #   @orderitems.each do |orderitem|
+  #     if allorders.has_key?(orderitem.order_id)
+  #       allorders[order_id] << orderitem
+  #     else
+  #       allorders[order_id] = [orderitem]
+  #     end
+  #   end
+  #
+  #   return allorders
+  # end
 
-    return merchant
-  end
 end
