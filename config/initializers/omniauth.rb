dd4d3e5d8815bb6b7c7e9ce4f0e6490b32e7acb4
Pauline Chane <pauline.chane@gmail.com>
2020-11-18 16:22:31 -0800

    minor edit to initializer to add two options for when oauth is implemented

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
 # config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"], scope: "user:email"
+  provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"], scope: "merchant:email"
+  # one of these should work, not sure which one
+  # provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"], scope: "user:email"
 end
