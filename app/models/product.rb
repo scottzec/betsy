b09db45fb44401e74bd640ff1166b91f1f8ce4ae
Pauline Chane <pauline.chane@gmail.com>
2020-11-18 22:37:00 -0800

    fixed category/product relations, working on show view for categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  has_and_belongs_to_many :categories
+  has_and_belongs_to_many :categories, :join_table => :categories_products_joins # added this for relation to work
   has_many :orderitems
 
   # Custom Method: Retire a product from being sold, which hides it from browsing
