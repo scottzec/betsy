b24f8d76a1e0b737251841cc3efdb9814213cdb2
Pauline Chane <pauline.chane@gmail.com>
2020-11-20 03:30:14 -0800

    untested custom model methods

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -16,36 +16,46 @@ class Merchant < ApplicationRecord
   #   return merchant
   # end
   #
-  # # total revenue based on completed orders
-  # def total_revenue
-  #   @orderitems = self.orderitems
-  #   return 0 if @orderitems.empty?
-  #
-  #   total = 0
-  #   @orderitems.each do |orderitem|
-  #     order = Order.find_by(id: orderitem.order_id)
-  #     product = Product.find_by(id: orderitem.product_id)
-  #     if order.status.downcase == 'completed'
-  #       total += orderitem.quantity * price
-  #     end
-  #   end
-  #
-  #   return total
-  # end
-  #
-  # def all_orders
-  #   @orderitems = self.orderitems
-  #   return nil if @orderitems.empty?
-  #   allorders = Hash.new
-  #   @orderitems.each do |orderitem|
-  #     if allorders.has_key?(orderitem.order_id)
-  #       allorders[order_id] << orderitem
-  #     else
-  #       allorders[order_id] = [orderitem]
-  #     end
-  #   end
-  #
-  #   return allorders
-  # end
+
+  # total revenue based on completed orders
+  def total_revenue(status: nil)
+    @orderitems = self.orderitems
+    return 0 if @orderitems.empty?
+
+    total = 0
+    @orderitems.each do |orderitem|
+      order = Order.find_by(id: orderitem.order_id)
+      product = Product.find_by(id: orderitem.product_id)
+      # if no status input, will total everything.
+      # if status input, will match by status
+      if status.nil? || order.status.downcase == status
+        total += orderitem.quantity * product.price
+      end
+    end
+
+    return total
+  end
+
+  # all unique orders and order items associated with merchant, can be sorted by status
+  def all_orders(status: nil)
+    orderitems = self.orderitems
+    return nil if orderitems.empty?
+    
+    allorders = Hash.new
+    orderitems.each do |orderitem|
+      order = Order.find_by(id: orderitem.order_id)
+      # if no status input, will collect everything.
+      # if status input, will match by status
+      if status.nil? || order.status.downcase == status
+        if allorders.has_key?(orderitem.order_id)
+          allorders[order_id] << orderitem
+        else
+          allorders[order_id] = [orderitem]
+        end
+      end
+    end
+
+    return allorders
+  end
 
 end
