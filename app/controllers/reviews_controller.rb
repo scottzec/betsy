1c915fea40f30b5ddac106dee7cb5f4917ca6bd8
Pauline Chane <pauline.chane@gmail.com>
2020-11-20 03:01:29 -0800

    added those extra reviews tests

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,14 +16,25 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @product = Product.find_by(id: params[:product_id])
-    @merchant = Merchant.find_by(id: @product.merchant_id)
-    @review = @product.reviews.new(review_params)
-    if @review.save
-      flash[:success] = "successfully left review of #{@product.name} by #{@merchant.username}"
-      redirect_to product_path(@product.id)
+    if !Product.find_by(id: params[:product_id]).nil?
+      @product = Product.find_by(id: params[:product_id])
+      @merchant = Merchant.find_by(id: @product.merchant_id)
+      # the page is still accessible, for those tricky merchants that try to find the page manually
+      if(!session[:user_id].nil? && session[:user_id] == @product.merchant_id)
+        flash[:warning] = "you can't review your own product!"
+        redirect_to product_path(@product.id)
+        return
+      end
+      @review = @product.reviews.new(review_params)
+      if @review.save
+        flash[:success] = "successfully left review of #{@product.name} by #{@merchant.username}"
+        redirect_to product_path(@product.id)
+      else
+        render :new, status: :bad_request
+      end
     else
-      render :new, status: :bad_request
+      flash[:warning] = "no product found - feel free to review our other products below!"
+      redirect_to root_path
     end
     return
   end
