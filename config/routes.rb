bbfc6b1456190f9e26a230d8f9700f64eb81b2c2
roshni-patel <rpatel01@wesleyan.edu>
2020-11-20 01:30:20 -0600

    Updated checkout method, cart, show page for merchant, and Pauline's dashboard (need to do for tomorrow)

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   get "/cart", to: "orders#cart", as: "cart"
   get "/orders/:id/merchant_show", to: "orders#merchant_show", as: "merchant_show"
 
-  resources :orders   # , only: [:show, :edit, :update]
+  resources :orders   # , only: [:show, :edit, :update, :destroy]
 
   # for categories
   resources :categories, only: [:index, :show, :new, :create]
