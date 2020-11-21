fa9f10fdc88685dac6eddd84340222e168853c57
Pauline Chane <pauline.chane@gmail.com>
2020-11-20 17:33:03 -0800

    v0 of build from github method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,18 +4,21 @@ class Merchant < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  #
-  # def self.build_from_github(auth_hash)
-  #   merchant = Merchant.new
-  #   merchant.uid = auth_hash[:uid]
-  #   merchant.provider = "github"
-  #   # merchant can change name later
-  #   merchant.username = auth_hash["info"]["name"]
-  #   merchant.email = auth_hash["info"]["email"]
-  #
-  #   return merchant
-  # end
-  #
+
+  def self.build_from_github(auth_hash)
+    merchant = Merchant.new
+    merchant.uid = auth_hash[:uid]
+    merchant.provider = "github"
+    # merchant can change name later
+    merchant.provider_name = auth_hash["info"]["name"]
+    merchant.provider_email = auth_hash["info"]["email"]
+
+    merchant.username = merchant.provider_name
+    merchant.email = merchant.provider_email
+
+    return merchant
+  end
+
 
   # total revenue based on completed orders
   # all unique orders and order items associated with merchant, can be sorted by status
