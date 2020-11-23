acb193b816d0f322d38514d1a16cb5e9da07be77
Pauline Chane <pauline.chane@gmail.com>
2020-11-22 18:51:39 -0800

    working controller filter for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,44 +1,19 @@
 class ReviewsController < ApplicationController
   skip_before_action :require_login
+  before_action :get_merchant_and_product
   def new
-    if !Product.find_by(id:params[:product_id]).nil?
-      @product = Product.find_by(id: params[:product_id])
-      @merchant = Merchant.find_by(id: @product.merchant_id)
-      # the page is still accessible, for those tricky merchants that try to find the page manually
-      if(!session[:user_id].nil? && session[:user_id] == @product.merchant_id)
-        flash[:warning] = "you can't review your own product!"
-        redirect_to product_path(@product.id)
-      end
-      @review = @product.reviews.new
-    else
-      flash[:warning] = "no product found - feel free to review our other products below!"
-      redirect_to root_path
-    end
+    @review = @product.reviews.new
   end
 
   def create
-    if !Product.find_by(id: params[:product_id]).nil?
-      @product = Product.find_by(id: params[:product_id])
-      @merchant = Merchant.find_by(id: @product.merchant_id)
-      # the page is still accessible, for those tricky merchants that try to find the page manually
-      if(!session[:user_id].nil? && session[:user_id] == @product.merchant_id)
-        flash[:warning] = "you can't review your own product!"
-        redirect_to product_path(@product.id)
-        return
-      end
-      @review = @product.reviews.new(review_params)
-      if @review.save
-        flash[:success] = "successfully left review of #{@product.name} by #{@merchant.username}"
-        redirect_to product_path(@product.id)
-      else
-        flash[:error] = @review.errors.messages
-        render :new, status: :bad_request
-      end
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      flash[:success] = "successfully left review of #{@product.name} by #{@merchant.username}"
+      redirect_to product_path(@product.id)
     else
-      flash[:warning] = "no product found - feel free to review our other products below!"
-      redirect_to root_path
+      flash[:error] = @review.errors.messages
+      render :new, status: :bad_request
     end
-    return
   end
 
   private
