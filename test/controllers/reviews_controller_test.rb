feae5adf585e9d44e7fa8797f0d61136bfb11e7b
Pauline Chane <pauline.chane@gmail.com>
2020-11-23 15:49:19 -0800

    reviews controller test passing

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,34 +1,24 @@
 require "test_helper"
 # REFACTOR /ALL/ FOR OAUTH
 describe ReviewsController do
-  # before do
-  #   @merchant1 = Merchant.create(username: "m1", email: "m1@email.com")
-  #   @merchant2 = Merchant.create(username: "m2", email: "m2@email.com")
-  #   @product = Product.create(name: "review me", description: "an item", price: 1.00, photo_url:"/images/defaultimage.jpg", stock: 5, merchant_id: @merchant1.id)
-  # end
-  #
   describe "when signed in" do
+    before do
+      perform_login(merchants(:test))
+    end
     describe "new" do
-      before do
-      end
       it "allows a signed-in user that is not the merchant selling the product to go to the form to leave a review" do
-        @login_data = {merchant: {username: @merchant2.username, email:@merchant2.email} }
-        post login_path(@login_data) # REFACTOR
         # Act
-        get new_product_review_path(@product.id)
+        get new_product_review_path(products(:product3).id)
 
         # Assert
         must_respond_with :success
       end
       it "forbids the merchant selling the product to leave a review for it and redirects to the product page" do
-        @login_data = {merchant: {username: @merchant1.username, email: @merchant1.email} }
-        post login_path(@login_data) # REFACTOR
-
         # Act
-        get new_product_review_path(@product.id)
+        get new_product_review_path(products(:product1).id)
 
         # Assert
-        must_redirect_to product_path(@product.id)
+        must_redirect_to product_path(products(:product1).id)
       end
     end
 
@@ -37,25 +27,19 @@ describe ReviewsController do
         @review = {review: {rating: 5, review: "this should work"} }
       end
       it "lets a merchant review another product" do
-        @login_data = {merchant: {username: @merchant2.username, email:@merchant2.email} }
-        post login_path(@login_data) # REFACTOR
-
         expect{
-          post product_reviews_path(@product.id), params: @review
+          post product_reviews_path(products(:product3).id), params: @review
         }.must_change "Review.count", 1
 
-        must_redirect_to product_path(@product.id)
+        must_redirect_to product_path(products(:product3).id)
       end
       it "blocks a merchant from reviewing their own product" do
-        # yes, even if they try to do it through postman
-        @login_data = {merchant: {username: @merchant1.username, email:@merchant1.email} }
-        post login_path(@login_data) # REFACTOR
-
+        # # yes, even if they try to do it through postman
         expect{
-          post product_reviews_path(@product.id), params: @review
+          post product_reviews_path(products(:product1).id), params: @review
         }.wont_change "Review.count"
 
-        must_redirect_to product_path(@product.id)
+        must_redirect_to product_path(products(:product1).id)
       end
     end
   end
