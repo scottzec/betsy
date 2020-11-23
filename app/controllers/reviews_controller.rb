36b4fef7791aef2724797c75cb4df9e6d6851bd8
Pauline Chane <pauline.chane@gmail.com>
2020-11-22 18:46:42 -0800

    created controller filter, saving now incase i need to revert commit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -46,4 +46,20 @@ class ReviewsController < ApplicationController
   def review_params
     return params.require(:review).permit(:rating, :review)
   end
+
+  def get_merchant_and_product
+    if !Product.find_by(id: params[:product_id]).nil?
+      @product = Product.find_by(id: params[:product_id])
+      @merchant = Merchant.find_by(id: @product.merchant_id)
+      if(!session[:user_id].nil? && session[:user_id] == @product.merchant_id)
+        flash[:warning] = "you can't review your own product!"
+        redirect_to product_path(@product.id)
+        return
+      end
+    else
+      flash[:warning] = "no product found - feel free to review our other products below!"
+      redirect_to root_path
+      return
+    end
+  end
 end
