7e6f6938bc5a835603c9cb8fd9038eb9dee56871
Mackenzie <mvlofthus@gmail.com>
2020-11-23 17:52:28 -0800

    use yml in orderitems controller test, more updates to nav bar

--- test/controllers/orderitems_controller_test.rb
+++ test/controllers/orderitems_controller_test.rb
@@ -5,40 +5,16 @@ describe OrderitemsController do
   #   value(1+1).must_equal 2
   # end
   #
-  let (:product) {
-    Product.create!(name: "orchid",
-                   description: "pretty fragile flower",
-                   price: 21.00,
-                   photo_url: nil,
-                   stock: 30,
-                   merchant_id: 1)
-  }
-
-  let (:order) {
-    Order.create!(status: "paid",
-                 name: "Buy Buyerson",
-                 email: "buy@buy.com",
-                 address: "1234 buy st. s., Buy, OR",
-                 credit_card_number: "1234 5678 9101 1121",
-                 zip_code: "12345-1234",
-                 total: 84.00)
-  }
-
-  let (:orderitem) {
-    Orderitem.create!(quantity: 4,
-                     product_id: product.id,
-                     order_id: order.id)
-  }
 
   describe "show" do
-    # Your tests go here
-    it "responds with success when showing an existing valid trip" do
-      get orderitem_path(orderitem.id)
+    it "responds with success when showing an existing valid orderitem" do
+      oi1 = orderitems(:shipped1)
+      get orderitem_path(oi1.id)
 
       must_respond_with :success
     end
 
-    it "responds with redirect with an invalid trip id" do
+    it "responds with redirect with an invalid orderitem id" do
       get orderitem_path(-1)
 
       must_respond_with :redirect
@@ -46,6 +22,7 @@ describe OrderitemsController do
     end
 
   end
+
 
 
 end
