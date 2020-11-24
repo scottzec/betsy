5c3bbdac0c2ef3d39bec42ef55154242d5e0910f
Pauline Chane <pauline.chane@gmail.com>
2020-11-23 16:07:56 -0800

    logged out category controller tests passing

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,64 +1,70 @@
 require "test_helper"
 
 describe CategoriesController do
-  describe "index" do
-    it "responds with success when there are many categories saved" do
-      # Arrange
-      Category.create(name: "test")
-      # Act
-      get merchants_path
-      # Assert
-      must_respond_with :success
+  describe "logged out" do
+    describe "index" do
+      it "responds with success when there are many categories saved" do
+        get categories_path
+        # Assert
+        must_respond_with :success
+      end
+
+      it "responds with success when there are no categories saved" do
+        # Arrange
+        Category.delete_all
+        # Act
+        get categories_path
+        # Assert
+        must_respond_with :success
+      end
     end
 
-    it "responds with success when there are no categories saved" do
-      # Arrange
+    describe "show" do
+      it "responds with success when showing an existing valid category" do
+        # Act
+        get category_path(categories(:category1).id)
 
-      # Act
-      get merchants_path
-      # Assert
-      must_respond_with :success
+        # Assert
+        must_respond_with :success
+      end
+
+      it "responds with a redirect (returns success) to the categories page with an invalid id" do
+        # Act
+        get category_path(-1)
+
+        # Assert
+        must_redirect_to categories_path
+
+      end
+    end
+
+    describe "new" do
+      it "prevents non signed in users from visiting the page to make a new category" do
+        get new_category_path
+
+        must_redirect_to root_path
+      end
+    end
+
+    describe "create" do
+      it "blocks signed out merchants from making categories" do
+        @cat = {category: {name: "test"} }
+
+        expect{
+          post categories_path, params: @cat
+        }.wont_change "Category.count"
+
+        must_redirect_to root_path
+      end
     end
   end
 
-  describe "show" do
-    # Arrange
-    before do
-      Category.create(name: "test")
-    end
-    it "responds with success when showing an existing valid category" do
-      # Arrange
-      id = Category.find_by(name:"test")[:id]
-
-      # Act
-      get category_path(id)
-
-      # Assert
-      must_respond_with :success
-    end
-
-    it "responds with a redirect (returns success) to the categories page with an invalid id" do
-      # Act
-      get category_path(-1)
-
-      # Assert
-      must_redirect_to categories_path
-
-    end
-  end
-
-  describe "session functions" do
+  describe "logged in" do
     before do
       @merchant1 = Merchant.create(username: "m1", email: "m1@email.com")
     end
     # the following functions require login
     describe "new" do
-      # needs OAuth
-      it "prevents non signed in users from visiting the page to make a new category" do
-        get new_category_path
-
-        must_redirect_to categories_path
-      end
       it "allows signed in users to visit the page to make a new category" do
         @login_data = {merchant: {username: @merchant1.username, email:@merchant1.email} }
         post login_path(@login_data) # REFACTOR
@@ -73,15 +79,6 @@ describe CategoriesController do
         @login_data = {merchant: {username: @merchant1.username, email:@merchant1.email} }
       end
       # needs OAuth
-      it "blocks signed out merchants from making categories" do
-        @cat = {category: {name: "test"} }
-
-        expect{
-          post categories_path, params: @cat
-        }.wont_change "Category.count"
-
-        must_redirect_to categories_path
-      end
       it "creates a new valid category" do
         post login_path(@login_data)
 
