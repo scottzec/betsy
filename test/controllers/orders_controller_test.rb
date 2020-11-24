68fa8b3409d28d773231ec6289d6804d8cf3b05d
roshni-patel <rpatel01@wesleyan.edu>
2020-11-24 01:19:58 -0600

    Added orders controllers tests

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,13 +1,14 @@
 require "test_helper"
 
 describe OrdersController do
+  let(:order) do
+    Order.create(status: 'paid', name: "buyer", email: "buyer@email.com", address: "123 Ada Court", credit_card_number: '123456789', cvv: '123', expiration_date: '2/21', total: 20)
+  end
+
   before do
     @merchant = Merchant.create(username: 'test merchant', email: 'test_merchant@email.com', provider: "github", uid: 11111112)
     @product = Product.create(name: 'test plant', description: 'test description', price: 5.0, photo_url: "url.com/plant", stock: 6, merchant_id: @merchant.id)
-  end
-
-  let(:order) do
-     Order.create(status: 'paid', name: "buyer", email: "buyer@email.com", address: "123 Ada Court", credit_card_number: '123456789', cvv: '123', expiration_date: '2/21', total: 20)
+    @orderitem = Orderitem.create(quantity: 2, order_id: order.id, product_id: @product.id, shipped: false)
   end
 
   describe "show" do
@@ -31,12 +32,21 @@ describe OrdersController do
     it "responds with success if the merchant is logged in and the order contains a product of theirs" do
       perform_login(@merchant)
 
-
-
-
+      get merchant_show_path(id: order.id)
+      must_respond_with :success
     end
 
+    it "redirects to the root path if the merchant is not logged in" do
+      get merchant_show_path(id: order.id)
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
 
+    it "redirects to the root path if the order cannot be found" do
+      get merchant_show_path(id: -1)
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
   end
 
 
@@ -48,12 +58,40 @@ describe OrdersController do
   end
 
   describe "checkout" do
-    it "confirms the order if the cart can be checked out and redirects" do
+    before do
+      get cart_path
+    end
 
+    it "confirms the order if the cart can be checked out and redirects" do
+      orderitem = { orderitem: {quantity: 5, product_id: @product.id, shipped: false } }
+
+      post orderitems_path(orderitem)
+      # add an order item
+      order_params = {order: {status: "pending", name: "test", email: "test@email.com",
+                              address: "123 test lane", credit_card_number: "123456789", cvv: "123", expiration_date: "09/22", zip_code: "12345"} }
+      expect {
+        patch cart_path(order_params)
+      }.wont_change 'Order.count' # won't change because cart is created in before block
+
+      expect(flash[:success]).must_equal "Your order has been confirmed."
+      order = Order.last
+      must_redirect_to order_path(order.id)
     end
 
     it "cannot confirm the order if the cart cannot be checked out" do
+      orderitem = { orderitem: {quantity: 5, product_id: @product.id, shipped: false } }
 
+      post orderitems_path(orderitem)
+      # add an order item
+      order_params = {order: {status: "pending", name: nil, email: "test@email.com",
+                              address: "123 test lane", credit_card_number: "123456789", cvv: "123", expiration_date: "09/22", zip_code: "12345"} }
+      expect {
+        patch cart_path(order_params)
+      }.wont_change 'Order.count' # won't change because cart is created in before block
+
+      order = Order.last
+      must_respond_with :bad_request
+      expect(flash.now[:errors]).wont_be_nil
     end
   end
 
